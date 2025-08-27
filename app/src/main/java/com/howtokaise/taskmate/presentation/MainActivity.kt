a23d6f039263de11a2e98b3a43435a135c71d5ba
Adil Jawed <mdadiljawed786@gmail.com>
2025-08-27 17:57:58 +0530

    Initial Commit

---
+++ app/src/main/java/com/howtokaise/taskmate/presentation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.howtokaise.taskmate.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.howtokaise.taskmate.presentation.ui.theme.TaskMateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskMateTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
